0aff91d85ddfe76ab3acf07b4045f940bde1e858
Riz <80712058+rizznn@users.noreply.github.com>
2021-05-09 23:01:30 -0400

    functions of characters

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,25 @@
 // Assignment code here
+// var randomLength = function () {
+//     length = Math.random();
 
+//     length = parseInt(length * 100);
+//     length = (length % 7) + 6
+// }
+
+function randomLetters() {
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  return letters[Math.floor(Math.random()*letters.length)];
+}
+
+function randomNumbers() {
+  const numbers = "0123456789";
+  return numbers[Math.floor(Math.random()*numbers.length)];
+}
+
+function randomChar() {
+  const specialChar = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+  return specialChar[Math.floor(Math.random()*specialChar.length)];
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +35,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function writePassword() {
+  var promptLength = window.prompt("Please input a number from 8-128 for the password length")
+
+  var lengthOfP = +promptLength.value;
+  console.log(lengthOfP);
+  
+  var promptLowercase = window.prompt("Do you want to include lowercase characters? y/n");
+  
+  var promptUppercase = window.prompt("Do you want to include uppercase characters? y/n");
+  
+  var promptNumbers = window.prompt("Do you want to include numbers? y/n");
+  
+  var promptSpecialChar = window.prompt("Do you want to include special characters? y/n");
+}

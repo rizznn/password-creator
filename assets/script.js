cc3b8b02acc01636dc6d666098e8a2b08b276720
Riz <80712058+rizznn@users.noreply.github.com>
2021-05-10 06:54:22 -0400

    ffinal password output

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,28 @@
 // Assignment code here
+var array = [];
+var newPassword = "";
 
 function randomUpper() {
   const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  return upperLetters[Math.floor(Math.random()*upperLetters.length)];
+  return upperLetters;
+  // [Math.floor(Math.random()*upperLetters.length)];
 }
 function randomLower() {
   const lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
-  return lowerLetters[Math.floor(Math.random()*lowerLetters.length)];
+  return lowerLetters;
+  // [Math.floor(Math.random()*lowerLetters.length)];
 }
 
 function randomNumbers() {
   const randNumbers = '0123456789';
-  return randNumbers[Math.floor(Math.random()*randNumbers.length)];
+  return randNumbers;
+  // [Math.floor(Math.random()*randNumbers.length)];
 }
 
 function randomChar() {
   const special = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
-  return special[Math.floor(Math.random()*special.length)];
+  return special;
+  // [Math.floor(Math.random()*special.length)];
 }
 
 
@@ -39,24 +45,37 @@ function generatePassword() {
   var promptLength = parseInt(prompt("Please input a number from 8-128 for the password length"));
     
   var promptLowercase = confirm("Do you want to include lowercase characters?");
-  if (promptLowercase === 'true')  {
-    var options = getPasswordOptions();
+  if (promptLowercase === true )  {
+    // var options = getPasswordOptions();
+    array.push(randomLower())
   }
 
   var promptUppercase = confirm("Do you want to include uppercase characters?");
-  if (promptUppercase === 'true' ) {
-    var options = getPasswordOptions(); 
+  if (promptUppercase === true ) {
+    // var options = getPasswordOptions();
+    array.push(randomUpper())
   }
 
   var promptNumbers = confirm("Do you want to include numbers?");
-  if (promptNumbers === 'true' ) {
-    var options = getPasswordOptions(); 
+  if (promptNumbers === true ) {
+      // var options = getPasswordOptions();
+      array.push(randomNumbers())
   }
 
   var promptSpecialChar = confirm("Do you want to include special characters?");
-  if (promptSpecialChar === 'true' ) {
-    var options = getPasswordOptions();
+  if (promptSpecialChar === true ) {
+      // var options = getPasswordOptions();
+      array.push(randomChar())
   }
+
+  console.log(array.toString());
+
+  var userOptions = array.toString();
+
+  for(let i = 0; i < promptLength; i += 1) {
+    newPassword += userOptions[Math.floor(Math.random() * userOptions.length)]
+  }
+  console.log(newPassword);
 
   const passwordOptions = {
     promptLength: promptLength,
@@ -65,9 +84,10 @@ function generatePassword() {
     randNumbers: randomNumbers,
     special: randomChar
   }
-  return passwordOptions;
-  // console.log(passwordOptions,"passwordOptions");
-
+  // return passwordOptions;
+  console.log(passwordOptions,"passwordOptions");
+  
+  return newPassword;
 }
 
 // alert(`${options}`);
